e7bda93267f177252fe832d65edb80a9fb2eaeb4
HenriqueSzilagy <HenriqueSzilagy@gmail.com>
2024-09-19 22:00:08 -0300

    fixed

--- controllers/lesson1.js
+++ controllers/lesson1.js
@@ -1,12 +1,17 @@
+const saleteRoute = (req, res) => {
+  res.send("Salete Guglielmo");
+};
+
 const henriqueRoute = (req, res) => {
-  res.send("Henrique");
+  res.send("Henrique Szilagy");
 };
 
 const lucasRoute = (req, res) => {
-  res.send("Lucas");
+  res.send("Lucas Szilagy");
 };
 
 module.exports = {
+  saleteRoute,
   henriqueRoute,
   lucasRoute,
 };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const routes = require('express').Router();
 const lesson1Controller = require('../controllers/lesson1');
  
-routes.get('/', lesson1Controller.henriqueRoute);
+routes.get('/', lesson1Controller.saleteRoute);
+routes.get('/henrique', lesson1Controller.henriqueRoute);
 routes.get('/lucas', lesson1Controller.lucasRoute);
 
 module.exports = routes;
